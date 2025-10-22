1257ef120476a32e93a6276d4a2ab1c0618c146f
Roland Mackintosh <rolandmackintosh1@gmail.com>
2025-10-22 16:04:14 -0700

    add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/hci-4.2/',
+   base: '/hci-solo-4.3-branch/',
 })
