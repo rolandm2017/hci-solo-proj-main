9dd117af7d858e6491c3e7787acaccf53c282cee
Roland Mackintosh <rolandmackintosh1@gmail.com>
2025-10-22 16:12:02 -0700

    update page base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/hci-solo-4.3-branch/',
+   base: '/hci-solo-4.2-branch/',
 })
